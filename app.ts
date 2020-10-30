c1cf8817c01fbf32ce0dbb8f32007dd43fb0c754
yxj0312 <yanxiajun@hotmail.com>
2020-10-30 23:22:56 +0100

    Literal Types

--- app.ts
+++ app.ts
@@ -1,14 +1,16 @@
-function combine(input1: number | string, input2: number | string) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+function combine(input1: number | string, input2: number | string, resultConversion: string) {
+    let result:any;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString;
     }
+
     return result;
 }
 
-const combineAges = combine(30, 60);
-console.log(combineAges);
+const combinedAges = combine(30, 60, 'as-number');
+console.log(combinedAges);
+const combinedStringAges = combine('30', '60', 'as-number');
 
-const combinedName = combine('Max', 'Anna');
+const combinedName = combine('Max', 'Anna', 'as-text');
