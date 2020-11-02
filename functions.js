580fec8fded5d2aaa019f8b7b9f7f6feebde60e7
yxj0312 <yanxiajun@hotmail.com>
2020-11-02 20:45:32 +0100

    libs

--- functions.js
+++ functions.js
@@ -6,13 +6,13 @@ function add(n1, n2) {
 //     console.log('Result: ' + num);
 // }
 function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
+    const result = n1 + n2;
     cb(result);
 }
 // printResult(add(5,12));
-var combineValues;
+let combineValues;
 combineValues = add;
-addAndHandle(10, 20, function (result) {
+addAndHandle(10, 20, (result) => {
     console.log(result);
 });
 // Error:
