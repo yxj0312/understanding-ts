52cb31ad44a355775b6eb4643988d386308a5d52
yxj0312 <yanxiajun@hotmail.com>
2020-11-03 23:26:59 +0100

    More options

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 // ! acts as an if check for button
 const button = document.querySelector('button')!;
 
+// noImplicitReturns
+function add1(n1:number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+}
 
-button.addEventListener('click', () => {
+function clickHandler(message: string) {
+    // noUnusedLocals
+    let userName = "Max";
     console.log('Clicked!');
-});
+};
+
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, "You're welcome"));
+}
